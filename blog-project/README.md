f7de6d7dc596ac8ddbe3e6f2ebd3931f947bed1c
anasa <anas-aji-seeroo>
2023-05-04 18:52:17 +0100

    blogsite project added

---
+++ blog-project/README.md
@@ -0,0 +1,18 @@
+
+# Blogging Site Project
+<img src="https://github-readme-tech-stack.vercel.app/api/cards?title=Technologies%20Used&align=center&lineCount=2&theme=github_dark&line1=node.js,node.js,4d600d;mongodb,mongodb,4d600d;EJS,EJS,4d600d;express,express,fde25a;&line2=html,html,7c42f9;css,css,8f207a;" alt="My Tech Stack" />
+
+<div align="justify">
+
+This blogging project was developed using **Node.js**, **Express.js**, **Mongo DB**, **EJS**, **Bootstrap**, **HTML**, and **CSS**. The project aims to provide a simple UI to signup users for mailing list.
+
+The app includes the following features:
+
+- Compose: Users can compose blog posts using a simple and user-friendly interface.
+- Save: Blog posts are saved to a database, ensuring that they are secure and can be accessed later.
+- Read: Users can read blog posts from the database and view them in a clean and organized format.
+- Find: Users can search for blog posts by blog id, making it easy to find specific topics.
+
+The front-end of the app was developed using EJS, while the back-end was built using Node.js and MongoDB.
+
+</div>
---
+++ blog-project/app.js
@@ -0,0 +1,112 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+connectToDB().catch(err => console.log(err));
+var dbConnection = false;
+var db = null;
+//schema for blog post
+const blogPostSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+//collection for blog posts
+const blogPost = mongoose.model("Post", blogPostSchema);
+
+
+app.get("/", async function(req, res){
+  if(dbConnection!=true){
+    connectToDB();
+  }
+  posts = await blogPost.find();
+
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+async function connectToDB() {
+  db = await mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  dbConnection = true;
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", async function(req, res){
+  try{
+    const post = new blogPost({
+      title: req.body.postTitle,
+      content: req.body.postBody
+    });
+  
+    await post.save(function(err){
+
+      if (!err){
+        console.log("post added");
+
+        res.redirect("/");
+   
+      }
+      else{
+        console.log(err);
+      }
+   
+    });
+  
+  }
+  catch (e){
+    console.log(e);
+  }
+  
+
+});
+
+app.get("/posts/:postId", async function(req, res){
+  const requestedPostId = req.params.postId;
+  try{
+    var currentPost = await blogPost.findOne({_id: requestedPostId});
+    res.render("post", {
+ 
+      title: currentPost.title,
+ 
+      content: currentPost.content
+ 
+    });
+  }
+  catch(e){
+    console.log(e);
+  }
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
